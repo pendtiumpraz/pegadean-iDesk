76037320221ddc7afad347a1b55420699450cbd8
Pegadean Dev <development@privasimu.com>
2026-05-09 16:25:13 +0700

    chore: blade root layout

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title inertia>{{ config('app.name', 'iDesk Compliance') }}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
+        @routes
+        @viteReactRefresh
+        @vite(['resources/js/app.jsx'])
+        @inertiaHead
+    </head>
+    <body class="antialiased">
+        @inertia
+    </body>
+</html>
